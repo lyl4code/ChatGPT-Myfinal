88abdf9007d7f5ea511f4e4d515ce71757024226
lyl4code <lyl4code@gmail.com>
2024-06-24 14:04:58 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           文书批改助手Pro
         </div>
       <div className={styles["sidebar-sub-title"]}>
-        为爱奉献版🐶🐶🐶🐶哈哈哈哈哈哈
+        为爱奉献版🐶🐶🐶能用到7月23日
       </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
