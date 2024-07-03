77b44a27d3862cbe9455d0f8a8ee48965be5a9db
lyl4code <lyl4code@gmail.com>
2024-07-04 03:07:01 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -127,6 +127,15 @@ export const DEFAULT_MODELS = [
     },
   },
   {
+    name: "gpt-4o",
+    available: true,
+    provider: {
+      id: "openai",
+      providerName: "OpenAI",
+      providerType: "openai",
+    },
+  },
+  {
     name: "gpt-3.5-turbo",
     available: true,
     provider: {
